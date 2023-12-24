36aadb82f9778779a794deb6cb35ff36095d07d4
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-24 04:30:05 +0000

    Completed basics of datatypes and string functions

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Basics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Longest int : {int.MaxValue}");
+        Console.WriteLine($"Smallest int : {int.MinValue}");
+        Console.WriteLine($"Longest long : {long.MaxValue}");
+        Console.WriteLine($"Smallest long : {long.MinValue}");
+        Console.WriteLine($"Longest decimal : {decimal.MaxValue}");
+        Console.WriteLine($"Smallest decimal : {decimal.MinValue}");
+        Console.WriteLine($"Longest double : {double.MaxValue}");
+        Console.WriteLine($"Smallest double : {double.MinValue}");
+        Console.WriteLine($"Longest float : {float.MaxValue}");
+        Console.WriteLine($"Smallest float : {float.MinValue}");
+        decimal decPi = 3.1413124324324325432M;
+        decimal decHigh = 31232432423523563634634.1242938947932743473242912343423423M;
+        Console.WriteLine($"{decPi + decHigh}");
+
+        //TypeCasting
+        bool boolFromStr = bool.Parse("false");
+        int intFromStr = int.Parse("2018");
+        double doubleFromStr = double.Parse("123.45");
+        string stringFromBool = doubleFromStr.ToString();
+        Console.WriteLine($"Datatype: {stringFromBool.GetType()}");
+        Console.WriteLine($"Double to int: {(int)doubleFromStr}");
+        Console.ReadLine();
+    }
+}
+
---
+++ StringBasics/StringBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace StringBasics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string randString = "This is a string";
+        Console.WriteLine($"Length of the string: {randString.Length}");
+        Console.WriteLine($"String contains is: {randString.Contains("is")}");
+        Console.WriteLine($"Index of is : {randString.IndexOf("is")}");
+        Console.WriteLine($"Remove String: {randString.Remove(10,6)}");
+        Console.WriteLine($"Replace string: {randString.Replace("string", "sentence")}");
+        Console.WriteLine($"Compare: {String.Compare("A", "a", StringComparison.OrdinalIgnoreCase)}");
+        Console.WriteLine($"Pad Left: {randString.PadLeft(20, '-')}");
+        Console.WriteLine($"Pad Right: {randString.PadRight(20, '-')}");
+        Console.WriteLine($"Pad Left and Right: {randString.PadRight(20, '-').PadLeft(24,'.')}");
+        Console.WriteLine($"To upper: {randString.ToUpper()}");
+        Console.WriteLine($"To lower: {randString.ToLower()}");
+        string newString = String.Format("{0} saw a {1} eating {2} in the {3}", "Peter", "rabbit", "carrot", "field");
+        Console.Write(newString + "\n");
+        Console.WriteLine(@"Exactly what I typed \n");
+        Console.ReadLine();
+    }
+}
+
